ccedaa633a3fdfdbefe32d881c322f8f68fbb551
Ralph.Vitug <rralph@rrc.ca>
2026-01-21 13:29:30 -0600

    add: portfolioPerformance variables

---
+++ src/portfolio/portfolioPerformance.ts
@@ -0,0 +1,18 @@
+// Given variables
+/**
+ * @param initialInvestment - amount of investment
+ * @param currentValue - amount of current value
+ * @param profitOrLoss - indication of profit or loss
+ * @param precentageChange - percentage of change/movement
+ * @param performanceSummary - summary of overall performance
+ */
+
+export interface portfolioPerformance {
+    initialInvestment: number;
+    currentValue: number;
+    profitOrLoss: number;
+    precentageChange: number;
+    performanceSummary: string;
+
+}
+
